45df630d4a9dc94c0322eee0b074576e786531ad
banma1234 <banma1234@gmail.com>
2023-04-09 15:11:24 +0900

    feat($number/decimal) : 에라토스테네스의 채를 이용한 소수판별 알고리즘 작성

---
+++ number_theory/decimal/decimal.js
@@ -0,0 +1,21 @@
+const decimal = (number) => {
+    // 크기가 number+1인 array를 선언함과 동시에 모든 원소를 true로 채우되 0, 1은 false로 변경
+    let result = Array(number + 1).fill(true).fill(false, 0, 2);
+
+    // 가장 작은 소수인 2부터 i^2가 number보다 크지 않을때 까지 반복
+    for (let i=2; i*i<=number; i++) {
+        if (result[i]) {
+            for (let j=i*i; j<=number; j+=i) {
+                result[j] = false;
+                /* result[i]가 true(=소수)일 경우 i ~ number까지
+                i의 배수를 모두 false로 전환, 해당 로직을 반복한다. */
+            }
+        }
+    }
+
+    // 0 제거
+    result.splice(0, 1);
+    return result;
+}
+
+console.log(decimal(17));
